4ceb75730b3bc3c04d5d3369cd30538e5eacb648
B-Musick <bmuze1@gmail.com>
2019-07-18 16:56:32 -0600

    Marked added to Previewer, takes text from props, uses marked to markdown and uses dangerouslySetInnerHTML to display

--- FRONT_END_LIRARIES/FRONT_END_LIBRARY_PROJECTS/MARKDOWN_PREVIEWER/markdown-editor/src/components/Previewer.js
+++ FRONT_END_LIRARIES/FRONT_END_LIBRARY_PROJECTS/MARKDOWN_PREVIEWER/markdown-editor/src/components/Previewer.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import marked from 'marked';
+
 
 class Previewer extends React.Component {
     state={}
-    
+
+    rawMarkup = () => {
+        let rawMarkup = marked(this.props.text, { sanitize: true });
+        return { __html: rawMarkup };
+    }
+
     render(){
         return (
             // Previewer - shows html form of textEditor text
-            <div id="previewer">{this.props.text}</div>
+            <div id="previewer" dangerouslySetInnerHTML={this.rawMarkup()} />
         )
     }
 }
--- FRONT_END_LIRARIES/FRONT_END_LIBRARY_PROJECTS/MARKDOWN_PREVIEWER/markdown-editor/src/components/TextEditor.js
+++ FRONT_END_LIRARIES/FRONT_END_LIBRARY_PROJECTS/MARKDOWN_PREVIEWER/markdown-editor/src/components/TextEditor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class TextEditor extends React.Component{
     state = {text: " "};
 
