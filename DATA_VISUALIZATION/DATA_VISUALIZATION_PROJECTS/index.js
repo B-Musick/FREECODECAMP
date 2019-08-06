1cfd9b73f627901c2c3143de74d01f84c42ffdb8
B-Musick <bmuze1@gmail.com>
2019-08-06 11:33:50 -0600

    x-axis and y-axis work, have ticks, but bars dont show (x and y attributes are on rect though

--- DATA_VISUALIZATION/DATA_VISUALIZATION_PROJECTS/index.js
+++ DATA_VISUALIZATION/DATA_VISUALIZATION_PROJECTS/index.js
@@ -11,7 +11,7 @@ req.onload = () => {
 
     const width = +(svg.attr('width'));
     const height = +(svg.attr('height'));
-    const padding = 30;
+    const padding = 80;
     // Push all the GDP data from Federal reserve
     json = JSON.parse(req.responseText);
     
@@ -21,53 +21,53 @@ req.onload = () => {
     })
     
     
-    let dates = dataset.map(data=>{
+    let dataMap = dataset.map(data=>{
         console.log(...data[0].match(/\d+/g));
-        return new Date(...data[0].match(/\d+/g));
+        return [new Date(...data[0].match(/\d+/g)),data[1]];
     })
     //console.log(dataset[0][0].match(/\d+/g))
-    console.log(dates)
+    console.log(dataMap)
     // Set the xScale using date values, map the domain to the range to fit the page
     let xScale = d3.scaleTime()
-        .domain([dates[0], dates[dates.length-1]])
-        // .domain(
-        //     [
-        //         new Date(d3.min(dataset, (d) => d[0])),
-        //         new Date(d3.max(dataset, (d) => d[0]))
-        //     ]
-        // )
+        // Take the domain 'dates' and map them to the x-axis
+        .domain([dataMap[0][0], dataMap[dataMap.length-1][0]]) // (first(earliest) date, last(latest) date)
         .range([padding, width - padding]);
         
-    // let xScale = d3.scaleLinear()
-    //     .range([padding, width - padding]);
-    console.log(xScale(new Date('1947 01 01')))
+    // console.log(xScale(new Date('1947 01 01'))); // How access dates in xScale
+
     // Set the y-scale using the GDP values
     let yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, d =>{
-            d[1]
-            // console.log(d[1])
-        } )])
+        .domain([0, d3.max(dataMap, (d) =>d[1] )])
+        /* y-axis starts at bottom of screen so need 
+         0 ------> height-padding(bottom), then d3.max(dataMap, d =>{d[1]})]) -----> padding (top) */
+
         .range([height - padding, padding]);
 
+    console.log(yScale('1000'))
     // Axis takes scale function, determine whaat values in scale correspond to what pixels
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
+
     svg.append('g')
-        .attr('transform', "translate(80, " + (height - padding) + ")") // translate from svg edge to bottom of screen
+        // Define x,y coordinates translation from the left of screen and from top of screen 
+        .attr('transform', "translate(0, " + (height - padding) + ")") // translate from svg edge to bottom of screen
         .call(xAxis) // Call function x-axis on elements of selection 'g'
         .attr('class', 'x-axis');
 
     svg.append('g')
-        .attr('transform', "translate(" + (height - padding) + ", 80)") // translate from svg edge to bottom of screen
+        // Translate will define location of y-axis by defining (x,y) translation
+        // If didnt add padding to x-coordinate, the y-axis is against the screen
+        .attr('transform', "translate(" + (padding) + ", 0)") // translate from svg left edge and y coordinate from top of screen
         .call(yAxis) // Call function x-axis on elements of selection 'g'
         .attr('class', 'y-axis');
+
     svg.selectAll('rect')
-        .data(dataset)
+        .data(dataMap)
         .enter()
         .append('rect')
-            .attr('x', d => xScale(d[0]))
-            .attr('y', d => yScale((d[1])))
-            .style('border', '1px solid black')
+            .attr('x', d => xScale(d[0])+"")
+            .attr('y', d => yScale((d[1]))+"")
+            .style('fill','black')
 
     svg.style('background-color', 'lightblue');
 
