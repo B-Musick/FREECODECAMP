a307b7f2e9cade36e77148c7eec38da49bdbbb85
B-Musick <bmuze1@gmail.com>
2019-08-06 10:57:47 -0600

    x-axis works, y-axis doesnt, nor do the bars

--- DATA_VISUALIZATION/DATA_VISUALIZATION_PROJECTS/index.js
+++ DATA_VISUALIZATION/DATA_VISUALIZATION_PROJECTS/index.js
@@ -1,3 +1,94 @@
-let svg = d3.select('svg');
 
-svg.style('background-color','lightblue');
+// API Request
+let req = new XMLHttpRequest();
+req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
+
+req.onload = () => {
+    const svg = d3.select('svg')
+
+    // dataset will be an array of arrays, the first value being the date, second the gdp
+    let dataset = []
+
+    const width = +(svg.attr('width'));
+    const height = +(svg.attr('height'));
+    const padding = 30;
+    // Push all the GDP data from Federal reserve
+    json = JSON.parse(req.responseText);
+    
+    json.data.forEach((val)=>{
+        dataset.push(val);    
+        
+    })
+    
+    
+    let dates = dataset.map(data=>{
+        console.log(...data[0].match(/\d+/g));
+        return new Date(...data[0].match(/\d+/g));
+    })
+    //console.log(dataset[0][0].match(/\d+/g))
+    console.log(dates)
+    // Set the xScale using date values, map the domain to the range to fit the page
+    let xScale = d3.scaleTime()
+        .domain([dates[0], dates[dates.length-1]])
+        // .domain(
+        //     [
+        //         new Date(d3.min(dataset, (d) => d[0])),
+        //         new Date(d3.max(dataset, (d) => d[0]))
+        //     ]
+        // )
+        .range([padding, width - padding]);
+        
+    // let xScale = d3.scaleLinear()
+    //     .range([padding, width - padding]);
+    console.log(xScale(new Date('1947 01 01')))
+    // Set the y-scale using the GDP values
+    let yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, d =>{
+            d[1]
+            // console.log(d[1])
+        } )])
+        .range([height - padding, padding]);
+
+    // Axis takes scale function, determine whaat values in scale correspond to what pixels
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+    svg.append('g')
+        .attr('transform', "translate(80, " + (height - padding) + ")") // translate from svg edge to bottom of screen
+        .call(xAxis) // Call function x-axis on elements of selection 'g'
+        .attr('class', 'x-axis');
+
+    svg.append('g')
+        .attr('transform', "translate(" + (height - padding) + ", 80)") // translate from svg edge to bottom of screen
+        .call(yAxis) // Call function x-axis on elements of selection 'g'
+        .attr('class', 'y-axis');
+    svg.selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('rect')
+            .attr('x', d => xScale(d[0]))
+            .attr('y', d => yScale((d[1])))
+            .style('border', '1px solid black')
+
+    svg.style('background-color', 'lightblue');
+
+
+
+
+
+
+};
+console.log(req.response);
+req.send();
+
+// ${document}.ready(()=>{
+//     var api ='https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+
+//     $.getJSON(api,(data)={
+//         alert(data.)
+//     })
+// })
+// let response = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+// let data = await response.json();
+// console.log(data);
+
+
--- FRONT_END_LIRARIES/FRONT_END_LIBRARY_PROJECTS/CALCULATOR/calculator/src/components/App.js
+++ FRONT_END_LIRARIES/FRONT_END_LIBRARY_PROJECTS/CALCULATOR/calculator/src/components/App.js
@@ -15,16 +15,105 @@ class App extends React.Component {
             '/': (x, y) => x / y ,
             
         },
-        nextValNegative: false // Used to set value to negative
+        nextValNegative: false, // Used to set value to negative
+        decimalActivated: false, // Keeps track of if decimal button is pressed
+        decimalVal: 0, // Keeps track of the decimal value
+        previousDecimal: false // This will keep track of when decimal pressed, 
+                                // if there was a value already at the end of the array which the decimal is added to
 
     }
 
     onButtonClick = async (value,id) =>{
         let operationsState = this.state.operations;
        // Gets passed the value and id from the Button component when clicked (passed through Buttons)
+       if (id === 'decimal') {
+            //If decimal is pressed, then add decimal to the last value in array
+            if(!this.state.decimalActivated){
+                // If not already decimal actived (cant have two in a row)
+                if (typeof operationsState[operationsState.length - 1] === 'string' ||
+                    typeof operationsState[operationsState.length - 1] === 'undefined') {
+                    // If the last operation is a type of string then add 0 to the left of decimal,
+                    // and dont pull anything from the last array
+                    await this.setState(prevState => ({
+                        decimalActivated: true,
+                        decimalVal: "0."
+
+                    }));
+
+                } else {
+                    // If the last operation was a number, then well add the decimal to that number
+
+                    await this.setState(prevState => ({
+                        // 
+                        previousDecimal: true,
+                        decimalActivated: true,
+                        decimalVal: prevState.operations[prevState.operations.length - 1] += "."
+
+                    }));
+                }
+            }else{
+                // Otherwise do nothing if decimal activated already
+            }
+            
+
+            console.log(this.state.decimalVal)
+
+
+        }else if (this.state.decimalActivated && typeof value ==='string'){
+        //    If this is a decimal and another operation is pressed, then add decimal to array
+           
+            if(this.state.decimalVal[this.state.decimalVal.length-1]==='.'){
+                
+                // If there is no value added after the decimal, then add 0
+                await this.setState(prevState => ({
+                    decimalVal: parseFloat(prevState.decimalVal).toFixed(1)
+                }));
+            }
+           
+            if(this.state.previousDecimal){
+                // If the decimal was added to a value already in the array
+                // we need to remove the previous one and add the new value
+                // As well need to set the booleans to false
+                let newArray = [...this.state.operations];
+                // Need to splice out the old operator, then add in the new one
+                // Remember that the following would return the removed item if saved to variable
+                // so dont want to save it
+                newArray.splice(newArray.length - 1, 1, this.state.decimalVal);
+
+
+                await this.setState(prevState => ({
+                    // 
+                    previousDecimal: false,
+                    decimalActivated: false,
+                    operations: [...newArray, value]
+
+                }));
+                console.log(this.state.operations)
+
+            }else{
+                // If the decimal was clicked when there was no previous val, then 0 was added
+                // So we just need to add the decimal value to the previous state
+                await this.setState(prevState => ({
+                    previousDecimal: false,
+                    decimalActivated: false,
+                    operations: [...prevState.operations, this.state.decimalVal,value]
+                }));
+                console.log(this.state.operations)
+            }
+
+       }
+       else if(this.state.decimalActivated){
+           console.log('here')
+        //    If decimal is activated, need to add values to the current number
+           await this.setState(prevState => ({
+               decimalActivated: true,
+               decimalVal: prevState.decimalVal += value
+           }));
+           console.log(this.state.decimalVal)
+           
+       }
        
-       
-       if(id === 'equals'){
+       else if(id === 'equals'){
 
         //    Set the initial value of grandTotal to the first value in array
         var grandTotal = operationsState[0] ? operationsState[0] : 0; // Holds the final total
@@ -77,9 +166,16 @@ class App extends React.Component {
                 })
                 
             }else{
+                // Need to splice create a new array deep copy of state
+                let newArray = [...this.state.operations];
+                // Need to splice out the old operator, then add in the new one
+                // Remember that the following would return the removed item if saved to variable
+                // so dont want to save it
+                newArray.splice(newArray.length - 1, 1, value);
+
                 await this.setState(prevState => ({
-                // Splice out last operator
-                operations: [...prevState.operations.splice(prevState.length-1,1), value]
+                // Set the new operations array to the newArray with old operation removed
+                    operations: [...newArray]
                 }))
             }
 
@@ -122,122 +218,3 @@ export default App;
 
 
 
-// onButtonClick OLD CODE
-
-/** // Need async/await since it takes time to setState, otherwise the value
-        // wont be put in the array before it is needed
-        // 'value' comes from the Buttons Component which is from the Button onClick
-        
-        if(id==='equals'){
-            // If the total is wanted
-            let grandTotal = 0; // Holds the final total
-            let currentOperator = ""; // holds the current operator
-
-            // Variables for if multiple digits in a row
-            let multiDigits = false;
-            let multiDigitStart = 0;
-            let multiDigitEnd = 0;
-            // console.log(this.state.operations);
-            this.state.operations.forEach((val,index)=>{
-                if(index===0){
-                    // If first value in the array, then set this to the total
-                    grandTotal=val;
-                }else if(typeof val==='string'){
-                    // If value is an operator
-                    currentOperator=val;
-                }else{
-                    // If current value is an integer
-                    if(typeof this.state.operations[index+1]=== 'string' ){
-                    // If the next value isnt another integer
-                    console.log(multiDigits);
-                        if(multiDigits){
-                            
-                            let value = '';
-                            // If there are multiple digits and this is the last
-                            multiDigits = false;
-                            for(let i=multiDigitStart;i<=index;i++){
-                                value.concat(this.state.operations[i]);
-                                
-                            }
-                            // Duplicate code, make method
-                            if(currentOperator==='+'){
-                                grandTotal+=parseInt(value);
-                            }else if(currentOperator==='-'){
-                                grandTotal-=parseInt(value);
-                                
-                            }else if(currentOperator==='*'){
-                                grandTotal*=parseInt(value);
-                            }else if(currentOperator==='/'){
-                                grandTotal/=parseInt(value);
-                                
-                            }
-                        }
-                        // Duplicate code, make method
-                        else if(currentOperator==='+'){
-                            grandTotal+=val;
-                        }else if(currentOperator==='-'){
-                            grandTotal-=val;
-                            
-                        }else if(currentOperator==='*'){
-                            grandTotal*=val;
-                        }else if(currentOperator==='/'){
-                            grandTotal/=val;
-                            
-                        }
-                    }else{
-                     console.log(multiDigits)
-                        
-                        multiDigits = true;
-                        if(this.state.operations[index-1]==='string'){
-                            // if previous value an operator, set start to the 
-                            // current index which is the first digit
-                            multiDigitStart = index;
-                        }
-                        
-                    }
-                    // Add to the total using the associated operator from the object list
-                    
-                }
-                
-            });
-            
-            await this.setState(prevState => ({
-                // If equals is called, add the '=' and 'total' to end of the array
-                // so they can be printed to the screen
-                operations: [...prevState.operations, value, grandTotal],
-                total: grandTotal
-            }));
-
-        }
-        else if(id==="clear"){
-            // If person presses 'CE' then there are no operations
-            
-            await this.setState(
-                {operations:[0]}
-            )
-        // TODO: You will need to check if two operaters placed beside eachother
-        }else if(this.state.operations[0]===0 && (parseInt(value)!==NaN)){
-            // If no values in the array, and inputting the first value, we wannt to
-            // remove the 0 which initially in the array and input the first new value
-            this.setState({
-                operations:[value]}); 
-                
-        }else if (typeof value==='string' && typeof this.state.operations[this.state.operations.length-1]==='string'){
-            // If the current value is an operator, and the last value put into 
-            // the operations array is an operator, use the current one
-            console.log('replacing operator')
-            
-            await this.setState(prevState => ({
-                operations: [...prevState.operations.pop(), value]
-            }))
-
-        
-        }else if(!(this.state.operations[this.state.operations.length-1]==='string' && value===0) &&
-            !(this.state.operations.length===0 && value===0)){
-                
-            // If the first number is not going to be 0 then
-            // This will add the operations to the array if valid
-            await this.setState(prevState => ({
-                operations: [...prevState.operations, value]
-            }))
-        } */
